9169c890479ec6f28b1d1255d281887f03356114
HyunsooZo <bzhs1992@icloud.com>
2024-11-15 00:05:44 +0900

    feat (commit) : change method for fetching commit history
    - 기존 방식: Jsoup + https://github.com/users/$username/contributions
    - 변경 방식: Apache Batik + https://ghchart.rshah.org/$githubId
    - 이유: GitHub API에서 더 이상 SVG 형식을 제공하지 않음

--- src/main/kotlin/com/backend/commitoclock/application/service/CommitCheckService.kt
+++ src/main/kotlin/com/backend/commitoclock/application/service/CommitCheckService.kt
@@ -1,6 +1,5 @@
 package com.backend.commitoclock.application.service
 
-import com.backend.commitoclock.domain.model.User
 import com.backend.commitoclock.infrastructure.gateway.CommitGateway
 import org.springframework.stereotype.Service
 import java.time.LocalDate
@@ -11,9 +10,7 @@ class CommitCheckService(
     private val commitGateway: CommitGateway
 ) {
     fun hasCommittedToday(githubId: String): Int {
-        val todayDate = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
-        val commitData = commitGateway.fetchCommitData(githubId)
-        val todayCommitCount = commitData[todayDate] ?: 0
-        return todayCommitCount
+        val today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
+        return commitGateway.fetchCommitData(githubId, today)
     }
 }
--- src/main/kotlin/com/backend/commitoclock/infrastructure/gateway/GithubGateway.kt
+++ src/main/kotlin/com/backend/commitoclock/infrastructure/gateway/GithubGateway.kt
@@ -1,17 +1,27 @@
 package com.backend.commitoclock.infrastructure.gateway
 
-import org.jsoup.Jsoup
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory
+import org.apache.batik.util.XMLResourceDescriptor
 import org.springframework.stereotype.Component
+import java.net.URI
+import javax.xml.xpath.XPath
+import javax.xml.xpath.XPathConstants
+import javax.xml.xpath.XPathFactory
 
 @Component
 class CommitGateway {
-    fun fetchCommitData(username: String): Map<String, Int> {
-        val url = "https://github.com/users/$username/contributions"
-        val doc = Jsoup.connect(url).get()
-        return doc.select("rect").associate {
-            val date = it.attr("data-date")
-            val count = it.attr("data-count").toIntOrNull() ?: 0
-            date to count
-        }
+    fun fetchCommitData(
+        githubId: String,
+        date: String
+    ): Int {
+        val uri = URI("https://ghchart.rshah.org/$githubId")
+        val svgStream = uri.toURL().openStream()
+        val parser = XMLResourceDescriptor.getXMLParserClassName()
+        val factory = SAXSVGDocumentFactory(parser)
+        val document = factory.createDocument(uri.toString(), svgStream)
+        val xPath: XPath = XPathFactory.newInstance().newXPath()
+        val expression = "//*[@data-date='$date']/@data-score"
+        val result = xPath.evaluate(expression, document, XPathConstants.STRING) as String
+        return result.toIntOrNull() ?: 0
     }
 }
