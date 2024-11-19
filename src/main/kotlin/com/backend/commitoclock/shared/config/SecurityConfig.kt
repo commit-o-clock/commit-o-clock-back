638f9ac17b975bd94a7f471feb8121e196e0739f
Junnyjun <chbe5082@naver.com>
2024-11-19 21:12:41 +0900

    refactor : use extension fun

--- src/main/kotlin/com/backend/commitoclock/shared/config/SecurityConfig.kt
+++ src/main/kotlin/com/backend/commitoclock/shared/config/SecurityConfig.kt
@@ -3,23 +3,27 @@ package com.backend.commitoclock.shared.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfig {
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf { it.disable() }
-            .authorizeHttpRequests { requests ->
-                requests
-                    .requestMatchers(
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**"
-                    ).permitAll()
-                    .anyRequest().permitAll()
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain = http.csrf { it.disable() }
+            .authorizeHttpRequests { it
+                    .allowedPath()
+                    .anyRequestAllowed()
             }
             .formLogin { it.disable() }
             .httpBasic { it.disable() }
-        return http.build()
-    }
+        .build()
+
+    private fun AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry.allowedPath() =
+        this.requestMatchers(
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+        ).permitAll()
+
+    private fun AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry.anyRequestAllowed() =
+        this.anyRequest().authenticated()
 }
