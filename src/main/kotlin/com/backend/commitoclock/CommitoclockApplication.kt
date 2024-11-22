ec6f4c677a48a7c2964d94456202ce1e7c0c8cb4
HyunsooZo <bzhs1992@icloud.com>
2024-11-22 20:14:57 +0900

    feat (commitoclock) : add MongoDB env variable binding

--- src/main/kotlin/com/backend/commitoclock/CommitoclockApplication.kt
+++ src/main/kotlin/com/backend/commitoclock/CommitoclockApplication.kt
@@ -1,5 +1,6 @@
 package com.backend.commitoclock
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,11 @@ import org.springframework.boot.runApplication
 class CommitoclockApplication
 
 fun main(args: Array<String>) {
+    val dotenv = Dotenv.load()
+    System.setProperty("MONGO_DB_HOST", dotenv["MONGO_DB_HOST"])
+    System.setProperty("MONGO_DB_PORT", dotenv["MONGO_DB_PORT"])
+    System.setProperty("MONGO_DB_DATABASE", dotenv["MONGO_DB_DATABASE"])
+    System.setProperty("MONGO_DB_USERNAME", dotenv["MONGO_DB_USERNAME"])
+    System.setProperty("MONGO_DB_PASSWORD", dotenv["MONGO_DB_PASSWORD"])
     runApplication<CommitoclockApplication>(*args)
 }
